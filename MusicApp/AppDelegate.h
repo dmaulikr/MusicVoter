c54fb3abe0bb85b12b50213a09ea856245466655
rudraaiyar <rudra.aiyar@gmail.com>
2015-02-02 15:15:53 -0800

    Initial Commit

---
+++ MusicApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicApp
+//
+//  Created by Rudra Aiyar on 2/2/15.
+//  Copyright (c) 2015 Bosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
